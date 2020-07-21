adc1ce63e37b540022209f4510f93536666462fa
zjilani <zaidjilani.09@gmail.com>
2020-07-21 14:50:01 +0530

    Few Changes

--- src/app.js
+++ src/app.js
@@ -90,7 +90,7 @@ fastify.setErrorHandler(function (error, request, reply) {
 
 // Run the server!
 appconfig = config.server 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3001
 const host = '0.0.0.0';
 
 fastify.listen(PORT,host,function (err, address) {
